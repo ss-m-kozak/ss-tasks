1067e9bd9ca163054e04b1b3eccaf3b46113d912
ss-m-kozak <martakozak.mk.2001@gmail.com>
2019-10-06 14:21:28 +0300

    modified task_88a_b.py

--- task_88a_b.py
+++ task_88a_b.py
@@ -2,29 +2,25 @@
 # a) з'ясувати, чи входить цифра 3 в запис числа n^2
 # b) змінити порядок цифр числа n на зворотній
 
-def is_3_in_nn():
+def is_3_in_nn(number):
     '''з'ясувати, чи входить цифра 3 в запис числа n^2'''
-    try:
-        number=int(input("Enter number:"))
-        for i in range(len(str(number**2))):
-            if str(number**2)[i]=='3':
-                return True
-        return False
-    except ValueError:
-        return "Please enter the number"
+    if not isinstance(number, int):
+        raise ValueError("Please enter the number")
+    for i in range(len(str(number**2))):
+        if str(number**2)[i]=='3':
+            return True
+    return False
 
 
-def reverse_number():
+def reverse_number(number):
     '''змінити порядок цифр числа n на зворотній'''
-    try:
-        number=int(input("Enter number:"))
-        reversed_num=0
-        while number>0:
-            reversed_num=reversed_num*10+number%10
-            number=number//10
-        return reversed_num
-    except ValueError:
-        return "Please enter the number"
+    if not isinstance(number, int):
+        raise ValueError("Please enter the number")
+    reversed_num=0
+    while number>0:
+        reversed_num=reversed_num*10+number%10
+        number=number//10
+    return reversed_num
 
-# print(is_3_in_nn())
-# print(reverse_number())
+# print(is_3_in_nn(3))
+# print(reverse_number("jujj"))
